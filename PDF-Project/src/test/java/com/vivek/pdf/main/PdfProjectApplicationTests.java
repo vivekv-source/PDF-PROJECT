a430bf9d52b6bb7934c41284068671cf03cf20aa
vivekv-source <vivek726381@gmail.com>
2022-10-19 14:17:37 +0530

    h2 database created

---
+++ PDF-Project/src/test/java/com/vivek/pdf/main/PdfProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.pdf.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
